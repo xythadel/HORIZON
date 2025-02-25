2c25c0466340a643885f321ec8a67a41e3fdeb64
xythadel <xythadelg@gmail.com>
2025-02-26 00:04:05 +0800

    first commit

---
+++ resources/js/pages/Horizon.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Hello Horizon</h1>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('horizon', function () {
+    return Inertia::render('Horizon');
+})->name('horizon');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
