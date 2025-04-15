a3b783913dab2c74ebf6f16c942b90ea06fd22bf
A D M I N <the4thshinobijdgt@gmail.com>
2025-04-15 23:13:40 +0800

    ive added the routes ofr the mylearning,modulesLara and ModuleVue

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,17 @@ Route::get('test', function () {
 // Route::get('dashboard', function () {
 //     return Inertia::render('Dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/module-vue', function () {
+    return Inertia::render('settings/VueModules/ModuleVue');
+});
+
+Route::get('/module-lara', function () {
+    return Inertia::render('settings/LaraModules/ModuleLara');
+});
+
+Route::get('/my-learning', function () {
+    return Inertia::render('settings/VueModules/MyLearning');
+});
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
