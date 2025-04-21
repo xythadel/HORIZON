6077877d4393810d9791d3d4795d1bdc40b19208
A D M I N <the4thshinobijdgt@gmail.com>
2025-04-21 10:20:39 +0800

    deleted mylearning module

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,9 @@ Route::get('/modulelara', function () {
     return Inertia::render('settings/LaraModules/ModuleLara');
 });
 
-Route::get('/mylearning', [MyLearningController::class, 'index'])->name('mylearning');
+Route::get('/mylearning', function () {
+    return Inertia::render('MyLearning');
+})->name('mylearning');
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
