d883a52613c1c778582732df233c92166a8a06b3
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-04 21:44:41 +0800

    made changes to fix the website

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
+        vue(), // Add this line to enable Vue processing
         laravel({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/js/bootstrap.js',
             ],
             refresh: true,
         }),
