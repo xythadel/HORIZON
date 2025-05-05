8bcec8949e960ed441c8fae5fd9e096bc651dacd
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-05 09:56:36 +0800

    CREATED A SSEDER FOR ADMINAND USER

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        $this->call(AdminAndUserSeeder::class);
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
