21ec5b754a8ca61ff09f6185e09b2fc17d2e4268
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-06 20:48:46 +0800

    debuggin still

--- database/migrations/2025_03_03_135759_create_topics_table.php
+++ database/migrations/2025_03_03_135759_create_topics_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('topics', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->timestamps();
         });
--- database/migrations/2025_03_04_142318_create_courses_table.php
+++ database/migrations/2025_03_04_142318_create_courses_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('description');
-            $table->string('name');
+            $table->string('Vue');
+            $table->string('Laravel');
             $table->timestamps();
         });
     }
