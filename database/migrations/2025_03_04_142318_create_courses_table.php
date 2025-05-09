ab3141d2f329c0973f0a006eb8f474735dd1dc23
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-09 22:48:23 +0800

    removed a duplicate file

--- database/migrations/2025_03_04_142318_create_courses_table.php
+++ database/migrations/2025_03_04_142318_create_courses_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('description');
             $table->string('name');
-            $table->string('name');
             $table->timestamps();
         });
     }
