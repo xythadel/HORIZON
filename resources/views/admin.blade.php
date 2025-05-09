73b6966d27c553153d3aeb1576b22083b1ae29c6
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-09 15:08:02 +0800

    added a code for blade file admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,3 +7,17 @@
 @endsection
 
 @vite('resources/js/admin.js')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Panel</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <div id="app" class="p-6">
+        <course-manager></course-manager>
+    </div>
+</body>
+</html>
