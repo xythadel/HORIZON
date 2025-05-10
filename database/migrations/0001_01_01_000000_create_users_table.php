6b635a200ee85c6662a3fe2dc518b08ba07f429d
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-10 16:14:32 +0800

    added a role in thable users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('photo')->default("https://www.gravatar.com/avatar/");
+            $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
