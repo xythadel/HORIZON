8efbc7619aa567a0d6b13e619792fa9c4241e4d7
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-10 16:16:01 +0800

    re coded again for the role table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('photo')->default("https://www.gravatar.com/avatar/");
-            $table->string('role')->default('user');
+            $table->string('role')->default('user')->after('email_verified_at');
             $table->rememberToken();
             $table->timestamps();
         });
