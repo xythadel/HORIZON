96999be333837e48d527facd594c9787977bd15c
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-15 11:18:35 +0800

    setup for each interaction

--- app/Models/QuizUserInteraction.php
+++ app/Models/QuizUserInteraction.php
@@ -1,10 +1,12 @@
 <?php
 
+// app/Models/QuizUserInteraction.php
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class QuizUserInteraction extends Model
 {
-    //
+    protected $fillable = ['user_id', 'quiz_id', 'completed'];
 }
+
--- app/Models/TopicUserInteraction.php
+++ app/Models/TopicUserInteraction.php
@@ -1,10 +1,12 @@
 <?php
 
+// app/Models/TopicUserInteraction.php
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class TopicUserInteraction extends Model
 {
-    //
+    protected $fillable = ['user_id', 'topic_id', 'completed'];
 }
+
