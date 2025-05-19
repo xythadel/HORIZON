b78ae396ca2af1506e435a1b3f03332db22b7e79
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-20 07:30:58 +0800

    debugging

--- app/Http/Middleware/IsUser.php
+++ app/Http/Middleware/IsUser.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Support\Facades\Auth;
-
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class IsUser
@@ -13,14 +12,17 @@ class IsUser
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function handle($request, Closure $next)
-{
-    if (Auth::check() && Auth::user()->role === 'user') {
-        return $next($request);
-    }
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->role === 'user') {
+            return $next($request);
+        }
 
-    abort(403, 'Unauthorized');
-}
+        // Always return a response
+        return abort(403, 'Unauthorized');
+    }
 }
