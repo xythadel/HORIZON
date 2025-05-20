ef45d88a764e961f770f56b97f81330a20a5d237
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-20 11:12:52 +0800

    re coded the route

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,8 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-
-        // ✅ Add your custom middleware here
-        'is_admin' => \App\Http\Middleware\IsAdmin::class,
-        'is_user' => \App\Http\Middleware\IsUser::class,    ];
+        'isAdmin' => \App\Http\Middleware\IsAdmin::class,
+        'is_user' => \App\Http\Middleware\IsUser::class,   
+    ];
 }
