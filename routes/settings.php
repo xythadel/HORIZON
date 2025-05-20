bdf8e8b43c41b547f9a82fe64b7e59450a58f4dc
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-20 10:45:55 +0800

    renamed and debugging for the duplicated name

--- routes/settings.php
+++ routes/settings.php
@@ -13,7 +13,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('settings/password', [PasswordController::class, 'edit'])->name('password.edit');
-    Route::put('settings/password', [PasswordController::class, 'update'])->name('password.update');
+   Route::put('settings/password', [PasswordController::class, 'update'])->name('settings.password.update');
+
 
     Route::get('settings/appearance', function () {
         return Inertia::render('settings/Appearance');
