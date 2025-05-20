9cd973ed58e1ca4844ef90b20edf186e63f82a6a
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-20 10:55:37 +0800

    commented the duplicate

--- routes/settings.php
+++ routes/settings.php
@@ -13,7 +13,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('settings/password', [PasswordController::class, 'edit'])->name('password.edit');
-   Route::put('settings/password', [PasswordController::class, 'update'])->name('settings.password.update');
+    //Route::put('settings/password', [PasswordController::class, 'update'])->name('settings.password.update');
 
 
     Route::get('settings/appearance', function () {
