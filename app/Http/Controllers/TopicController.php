16284d21a036cd8ae51a9b7e20b17f75c5ce4fdc
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-22 07:41:30 +0800

    removed the course id

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -32,7 +32,7 @@ class TopicController extends Controller
     $validated = $request->validate([
         'title' => 'required|string|max:255',
         'content' => 'required|string',
-        'course_id' => 'required|integer|exists:courses,id',
+
     ]);
 
     $topic = new Topic();
