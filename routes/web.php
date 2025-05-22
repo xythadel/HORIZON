4412443573515e852cf2b91218dbf0cc551384cc
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-23 04:16:55 +0800

    interchanging of routes

--- routes/web.php
+++ routes/web.php
@@ -73,12 +73,13 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/complete-registration', [App\Http\Controllers\Auth\CompleteRegistrationController::class, 'submitForm']);
 });
 
-
-    //For google login 
-    Route::get('/auth/google', [SocialiteController::class, 'redirectToGoogle']);
+Route::get('/auth/google', [SocialiteController::class, 'redirectToGoogle']);
     Route::get('/auth/google/callback', [SocialiteController::class, 'handleGoogleCallback']);
-
-}); // <-- Close the middleware group
+}); 
 
 require __DIR__ . '/settings.php';
+Route::get('/login', function () {
+    return Inertia::render('/');
+});
 require __DIR__ . '/auth.php';
+ // <-- Close the middleware group
