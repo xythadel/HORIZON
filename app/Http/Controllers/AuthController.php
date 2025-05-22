97dd41086e47d6ce1c6e73c9d5039bc1817b11a6
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-22 20:22:14 +0800

    setup the sign in button

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+{
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|unique:users',
+        'password' => 'required|string|min:6|confirmed'
+    ]);
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+        'role' => 'user' // or whatever default role you want
+    ]);
+
+    return response()->json([
+        'message' => 'Registration successful',
+        'user' => $user
+    ]);
+}
     public function login(Request $request)
     {
         $credentials = $request->validate([
