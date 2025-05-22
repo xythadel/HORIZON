67339e800a86c65105b11635727d672175be47fa
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-22 21:42:59 +0800

    added the birthday table in schema

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('birthday');
             $table->string('photo')->default("https://www.gravatar.com/avatar/");
             $table->string('role')->default('user')->after('email_verified_at');
             $table->rememberToken();
