6bb77a13205d00e4889dbc7a3b866ef98879fd57
A D M I N <the4thshinobijdgt@gmail.com>
2025-05-24 21:44:42 +0800

    recoded the user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        return response()->json(User::all());
+        return response()->json(User::select('id', 'name', 'email', 'role', 'created_at')->get());
     }
     public function update(Request $request)
     {
